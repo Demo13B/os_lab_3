898a64cd91e37ab94a849cd331e24be420f2f4a4
TRSalikhov <trsalikhov@mai.education>
2023-11-11 10:48:17 +0300

    Added semaphore

--- calculator.cpp
+++ calculator.cpp
@@ -1,19 +1,30 @@
 #include <fcntl.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <iostream>
 
-auto main() -> int {
+auto main(int argc, char** argv) -> int {
     FILE* f = fopen("output.txt", "w");
     fprintf(f, "The sums are: ");
 
     int memoryd;
     memoryd = open("memory.txt", O_RDWR, 0666);
     char* buffer = (char*)mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, memoryd, 0);
+    close(memoryd);
+
+    sem_t* sem = sem_open(argv[1], O_CREAT, 0777, 0);  // Open semaphore from argument list
+
+    if (sem == SEM_FAILED) {
+        perror("Could not open semaphore");
+        return -1;
+    }
 
     int num = 0, sum = 0;
     size_t i = 0;
+
+    sem_wait(sem);  // Wait for semaphore to unlock and then lock it
+
     while (buffer[i] != -1) {
         if (buffer[i] != ' ' && buffer[i] != '\n') {
             num *= 10;
@@ -30,6 +41,10 @@ auto main() -> int {
         }
         ++i;
     }
+
+    sem_close(sem);
+
+    munmap(buffer, 1024);
 
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <iostream>
 
 auto main() -> int {
     int memoryd;
     memoryd = open("memory.txt", O_RDWR | O_CREAT | O_TRUNC, 0666);
-
     ftruncate(memoryd, 1024);
     char* buffer = (char*)mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, memoryd, 0);
+    close(memoryd);
+
+    sem_t* sem = sem_open("mmap_sem", O_CREAT, 0777, 0);  // Open semaphore
+
+    if (sem == SEM_FAILED) {
+        perror("Could not open semaphore");
+        return -1;
+    }
 
     // Forking the process
     int id = fork();
@@ -17,9 +25,7 @@ auto main() -> int {
     if (id == -1) {  // fork error
         return 2;
     } else if (id == 0) {  // child process
-
-        execlp("./calculator", "./calculator", NULL);
-
+        execl("./calculator", "./calculator", "mmap_sem", NULL);
         return 3;
     } else {  // parent process
         char c;
@@ -30,13 +36,19 @@ auto main() -> int {
             buffer[i++] = c;
             c = getchar();
         }
-
         buffer[i] = c;
 
-        std::cout << "Stopped reading";
+        sem_post(sem);  // Unlock semaphore
+        sem_close(sem);
+
+        munmap(buffer, 1024);
+
         int status;
         waitpid(0, &status, 0);  // waiting for child process to finish
 
-        return status;
+        if (status != 0) {
+            perror("Child process exited with an error");
+        }
+        return 0;
     }
 }
